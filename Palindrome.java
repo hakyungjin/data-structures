bcdb2e1c7012a7feac54b9f8c90efcc6de5bfeb3
hakyungjin <ha983885@snu.ac.kr>
2025-04-15 19:38:18 +0900

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -7,37 +7,40 @@ public class Palindrome {
     Stack stack;
     Queue queue;
     int max_len;
+    int length;
 
     public Palindrome(int max_len){
         stack=new Stack(max_len);
         queue=new Queue(max_len);
         this.max_len=max_len;
+        length=0;
 }
 
     public void newInt(int i){
         stack.push(i);
         queue.enqueue(i);
-
+        length++;
     }
 
     public void clear(){
         stack.clear();
         queue.clear();
-
+        length=0;
     }
 
     public int length(){
-        return stack.length();
+        return length;
 
     }
 
     public boolean palindromeCheck(){
-        int k=max_len/2;
+        int currentLength = length();
+        int k = currentLength/2;
         for(int i=0; i<k; i++) {
-        if(stack.pop()!= queue.dequeue()) {return false;}
+            if(stack.pop() != queue.dequeue()) {
+                return false;
+            }
         }
-
-        
         return true;
     }
 }
